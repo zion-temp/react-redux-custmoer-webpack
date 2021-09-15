e791ffba85c2c55b89fd46cd802d2768a3faa2df
lxl <1094519417@qq.com>
2021-09-15 11:17:38 +0800

    风格

--- src/index.tsx
+++ src/index.tsx
@@ -1,29 +1,37 @@
-import React,{FC,Suspense,useReducer} from "react";
+import React, { FC, Suspense, useReducer, useEffect } from "react";
 import ReactDOM from "react-dom";
-import { HashRouter, Switch, Route ,Redirect,Link,BrowserRouter as Router} from 'react-router-dom'
+import { HashRouter, Switch, Route, Redirect, Link, BrowserRouter as Router } from 'react-router-dom'
 
 import "@/index.less";
 
 import Darkmode from 'darkmode-js';
-import {Provider} from 'react-redux'
+import { Provider } from 'react-redux'
 import store from "./store/index";
 const Home = React.lazy(() => import('./components/home/index'));
 const About = React.lazy(() => import('./components/about/index'));
-const App:FC = () => {
-    new Darkmode().showWidget();
-
+import { optionsType } from './type/index'
+const App: FC = () => {
+    const option: optionsType = {
+        time: '0.5s', // default: '0.3s'
+        saveInCookies: true, // default: true,
+        label: '🌓', // default: ''
+        autoMatchOsTheme: true // default: true
+    }
+    useEffect(() => {
+        new Darkmode(option).showWidget();
+    }, [])
     return <>
-            <Router>
-                <Link to="/">首页</Link>
-                <Link to="/about">关于我们</Link>
-                <Switch>
-                	<Redirect exact from='/' to='/home' />
-                        <Suspense fallback={<div>加载中...</div>}>
-                            <Route path="/home" component={Home} />
-                            <Route path="/about" component={About} />
-                        </Suspense>
-                </Switch>
-            </Router>
+        <Router>
+            <Link to="/">首页</Link>
+            <Link to="/about">关于我们</Link>
+            <Switch>
+                <Redirect exact from='/' to='/home' />
+                <Suspense fallback={<div>加载中...</div>}>
+                    <Route path="/home" component={Home} />
+                    <Route path="/about" component={About} />
+                </Suspense>
+            </Switch>
+        </Router>
     </>
 };
 
@@ -32,14 +40,14 @@ ReactDOM.render(
     <Provider store={store}>
         <App />
     </Provider>
-, document.getElementById("root"));
+    , document.getElementById("root"));
 
 // 渐进式serviceworker
-if('serviceWorker' in navigator){
-    navigator.serviceWorker.register('/service-worker.js').then(()=>{
-     console.log('succcess')
-    }).catch(()=>{
-     console.log('feld')
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('/service-worker.js').then(() => {
+        console.log('succcess')
+    }).catch(() => {
+        console.log('feld')
     })
- 
+
 }
---
+++ src/type/index.d.ts
@@ -0,0 +1,17 @@
+export type GeographicType = {
+
+};
+
+export interface optionsType {
+    bottom?: string;
+    right?: string;
+    left?: string;
+    time?: string;
+    mixColor?: string;
+    backgroundColor?: string;
+    buttonColorDark?: string;
+    buttonColorLight?: string;
+    saveInCookies?: boolean;
+    label: string;
+    autoMatchOsTheme?: boolean;
+}
